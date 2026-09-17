25ab31ccf1df31e62e4d7e024b389197d2e9f13c
Dark-Louis <louis.solty747@gmail.com>
2026-09-17 21:31:32 +0200

    Aurion documents scraping - Added /aurion/documents and /aurion/documents/download routes - Added dynamic submenu discovery with three page structure parsers - Added support for DataGrid, SelectOneMenu, and Consulter download types

--- src/routes/aurion/index.ts
+++ src/routes/aurion/index.ts
@@ -2,5 +2,6 @@ import { loginRoute } from "./login/route";
 import { gradesRoute } from "./grades/route";
 import { planningRoute } from "./planning/route";
 import { absencesRoute } from "./absences/route";
+import { documentsRoute } from "./documents/route";
 
-export default { loginRoute, gradesRoute, planningRoute, absencesRoute };
+export default { loginRoute, gradesRoute, planningRoute, absencesRoute, documentsRoute };
